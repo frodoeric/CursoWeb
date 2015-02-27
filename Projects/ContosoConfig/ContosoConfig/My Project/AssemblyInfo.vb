193b181629e8ad43d09b841a3df15262639549fb
frodoeric <frodoeric@hotmail.com>
2015-02-27 17:07:01 -0300

    final do dia

---
+++ Projects/ContosoConfig/ContosoConfig/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("ContosoConfig")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("ContosoConfig")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("4545e788-e9b0-4b96-b012-6f17da90dd90")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ Projects/JavaScript/JavaScript/js/Script1.js
@@ -0,0 +1,95 @@
+﻿
+var button = document.getElementById("clickMe");
+button.onclick = function () {
+    var elemento = document.getElementById("destaque");
+    elemento.innerHTML = "Hello <strong>WORLD</strong>";
+};
+
+for (var i = 0; i < 500; i++) {
+
+    setTimeout(function () {
+        alert("glu glu");
+
+    }, 1000);
+
+    setTimeout(function () {
+        alert("iê iê!");
+
+    }, 1000);
+
+    setTimeout(function () {
+        alert("tetéia!");
+
+    }, 1000);
+
+}
+
+
+
+
+//(function () {
+//    "use strict";
+
+//    var objeto = {
+//        nome: "Fulano",
+//        sobrenome: "Souza",
+//        getFullname: function () {
+//            return this.nome = " " + this.sobrenome;
+//        }
+//    };
+
+//    console.log(objeto.getFullname());
+//    var nome = objeto["nome"];
+
+//    var array = ["banana", "abacate", "maca"];
+//    array.push("pera");
+//    array[4] = "jaca";
+
+//    var data = new Date(2015, 1, 1);
+
+//    var attendees = [
+//        {
+//            "name": "Eric Gruber",
+//            "currentTrack": "1"
+//        }
+//    ]
+
+//})();
+
+//TESTAR REFERÊNCIAS
+// instalar o CHUZTPAH para o usar o qunit
+
+
+
+//function add(x, y) {
+//    if (typeof x == "number" && typeof y === "number") {
+//        return x + y;
+
+//    }
+//    throw new RangeException();
+    
+//}
+
+
+
+//var x;
+
+//console.log("O tipo de x: " + typeof x);
+
+//x = 1;
+
+//console.log("O tipo de x: " + typeof x);
+
+//x = "banana";
+
+//console.log("O tipo de x: " + typeof x);
+
+//x = new Date(10 / 01 / 2015);
+
+//console.log("O tipo de x: " + typeof x);
+
+//x = true;
+
+//console.log("O tipo de x: " + typeof x);
+
+//document.write("Hello World");
