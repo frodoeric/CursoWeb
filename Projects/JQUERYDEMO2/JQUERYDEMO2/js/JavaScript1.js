99fdf410b1f732bb4573e64656d51ec55300c8d3
frodoeric <frodoeric@hotmail.com>
2015-03-03 13:36:01 -0300

    AJAX

---
+++ Projects/JQUERYDEMO2/JQUERYDEMO2/js/JavaScript1.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $("#meuDiv").css("background-color", "red");//referência e usa css dinâmicamente
+
+    $("h2").each(function () {
+        this.style.color = "red";//altera a cor para cada h2
+    });
+
+    $("#togleClassButton").click(function () {//pega o evento do botão caso clicado
+        $(this).toggleClass("alerta");//       utiliza a classe alerta do css
+    });
+
+
+});
