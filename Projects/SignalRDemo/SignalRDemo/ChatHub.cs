f12b656b16e0766122852c35333a0dfd5416bdea
frodoeric <frodoeric@hotmail.com>
2015-03-06 09:23:00 -0300

    final

---
+++ Projects/SignalRDemo/SignalRDemo/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRDemo
+{
+    public class ChatHub: Hub
+    {
+        public void Send(string nome, string mensagem)
+        {
+            Clients.All.broadcastMessage(nome, mensagem); //utiliza o Dynamics
+        }
+    }
+}
